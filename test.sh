2b0f293ecad6acd93f2a1af46966e9791e7d18b0
mdarius31 <darius31murariu@gmail.com>
2025-07-02 23:19:26 +0300

    test script fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cc --std=c99 -Wextra -Wall -Werror -g test.c -o test.x86_64
 cc -P -E --std=c99 -Wextra -Wall -Werror test.c > temp.c.tmp
 
-clang-format -i temp.c
+clang-format -i temp.c.tmp
 
 valgrind -s --leak-check=full ./test.x86_64
